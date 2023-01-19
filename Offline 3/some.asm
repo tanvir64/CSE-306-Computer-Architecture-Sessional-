33dd108c9642cbd167ed5c5c8217e3c4555098c0
Tanvir Raihan <traihan3130@gmail.com>
2023-01-19 15:48:36 +0600

    ALU implementation Done

---
+++ Offline 3/some.asm
@@ -0,0 +1,23 @@
+addi $t0, $zero, 105    
+
+subi $t1, $t0, 5      
+
+add $t3, $t0, $t1     
+
+sll $t0, $t3, 15
+
+lw $t1, 5($t0)
+
+push $t1
+
+sw $t1, 0($sp)
+subi $sp,$sp,1
+
+pop $t1
+lw $t1, 0($sp)
+addi $sp,$sp,1
+
+push 3($t0)
+lw $t0, 3($t0)
+sw $t0, 0($sp)
+subi $sp,$sp,1
